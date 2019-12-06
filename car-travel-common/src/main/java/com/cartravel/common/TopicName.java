5bbc33dd37a9b34aa59c9ae72d362c7e0bcfc37f
dcs <419787760@qq.com>
2019-12-06 11:03:34 +0800

    init

---
+++ car-travel-common/src/main/java/com/cartravel/common/TopicName.java
@@ -0,0 +1,13 @@
+package com.cartravel.common;
+
+/**
+ * 功能描述
+ *
+ * @author dcs
+ * @version 0.0
+ * @tparam T 构造泛型参数
+ * @note 一些值得注意的地方
+ * @since 2019/12/06 11:02
+ */
+public class TopicName {
+}
---
+++ car-travel-util/src/main/java/com/cartravel/util/JedisUtil.java
@@ -0,0 +1,23 @@
+package com.cartravel.util;
+
+import com.cartravel.common.Constants;
+import org.apache.log4j.Logger;
+import redis.clients.jedis.*;
+import redis.clients.jedis.BinaryClient.LIST_POSITION;
+import redis.clients.util.SafeEncoder;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 功能描述
+ *
+ * @author dcs
+ * @version 0.0
+ * @tparam T 构造泛型参数
+ * @note 一些值得注意的地方
+ * @since 2019/12/05 18:29
+ */
+public class JedisUtil {
+}
